6470b30ba32a60dc0957d50b2e625b7104aafe9b
hsynkmk <huseyinkaymak99@gmail.com>
2022-04-03 14:44:19 +0300

    Saving Changes Stage 1

--- Board Game/SettingsForm.cs
+++ Board Game/SettingsForm.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Security.Cryptography;
+
 namespace Board_Game
 {
     public partial class SettingsForm : Form
@@ -15,6 +20,43 @@ namespace Board_Game
         public SettingsForm()
         {
             InitializeComponent();
+
+            XDocument doc = XDocument.Load(@"UserData.xml");
+            foreach (XElement elem in doc.Descendants("user"))
+            {
+                if (elem.Element("username")?.Value == "alik" && elem.Element("password")?.Value == "DHPE2fUXbxKmZ+t5bF2fbTBptMk=")
+                {
+                    string a = elem.Element("difficulty").Value;
+                    int count = difficultyCheckedListBox.Items.Count;
+                    int index;
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        index = (int)Char.GetNumericValue(a[i]);
+                        if(index == 1)
+                            difficultyCheckedListBox.SetItemChecked(i, true);
+                    }
+                    a = elem.Element("shape").Value;
+                    count = shapeCheckedListBox.Items.Count;
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        index = (int)Char.GetNumericValue(a[i]);
+                        if (index == 1)
+                            shapeCheckedListBox.SetItemChecked(i, true);
+                    }
+
+                    a = elem.Element("color").Value;
+                    count = colorCheckedListBox.Items.Count;
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        index = (int)Char.GetNumericValue(a[i]);
+                        if (index == 1)
+                            colorCheckedListBox.SetItemChecked(i, true);
+                    }
+                }
+            }
         }
 
         private void difficultyCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -45,6 +87,11 @@ namespace Board_Game
         {
             this.Hide();
             new GameForm().Show();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
